542f9e64061727492c19a5611f0494d43a7cceca
dtbuild <dtbuild@rigil>
2026-09-25 11:22:38 +0000

    eed35eb774935867b361bd89a58eaa62cc64ab21 Release 6.1.1
    Sync to source repo @eed35eb774935867b361bd89a58eaa62cc64ab21

--- js/dataTables.fixedColumns.js
+++ js/dataTables.fixedColumns.js
@@ -1,4 +1,4 @@
-/*! FixedColumns 6.1.0 for DataTables
+/*! FixedColumns 6.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
@@ -399,7 +399,7 @@ class FixedColumns {
         return widths.slice(0, index).reduce((accum, val) => accum + val, 0);
     }
 }
-FixedColumns.version = '6.1.0';
+FixedColumns.version = '6.1.1';
 FixedColumns.classes = {
     bottomBlocker: 'dtfc-bottom-blocker',
     fixedEnd: 'dtfc-fixed-end',
